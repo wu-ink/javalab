dc95f5214a652b7ea5b99ce1fd9e6c98327547a5
ink <ink.wu@outlook.com>
2019-12-11 23:01:31 +0800

    advanced jpa for hibernate

--- src/main/java/wu/ink/learn/ch5/controller/JpaController.java
+++ src/main/java/wu/ink/learn/ch5/controller/JpaController.java
@@ -1,5 +1,7 @@
 package wu.ink.learn.ch5.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +21,34 @@ public class JpaController {
 	public User getUser(Long id) {
 		User user = jpaUserRepository.findById(id).get();
 		return user;
+	}
+	
+	@RequestMapping("/getUserById")
+	@ResponseBody
+	public User getUserById(Long id) {
+		User user = jpaUserRepository.getUserById(id);
+		return user;
+	}
+	
+	@RequestMapping("/findUsers")
+	@ResponseBody
+	public List<User> findUsers(String userName, String note) {
+		return jpaUserRepository.findUsers(userName, note);
+	}
+	
+	@RequestMapping("/findByUserNameLike")
+	@ResponseBody
+	public List<User> findByUserNameLike(String userName) {
+		List<User> userList = jpaUserRepository.findByUserNameLike("%" + userName + "%");
+		return userList;
+	}
+	
+	@RequestMapping("/findByUserNameLikeOrNoteLike")
+	@ResponseBody
+	public List<User> findByUserNameLikeOrNoteLike(String userName, String note) {
+		String userNameLike = "%" + userName + "%";
+		String noteLike = "%" + note + "%";
+		List<User> userList = jpaUserRepository.findByUserNameLikeOrNoteLike(userNameLike, noteLike);
+		return userList;
 	}
 }
--- src/main/java/wu/ink/learn/ch5/dao/JpaUserRepository.java
+++ src/main/java/wu/ink/learn/ch5/dao/JpaUserRepository.java
@@ -1,8 +1,17 @@
 package wu.ink.learn.ch5.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import wu.ink.learn.ch5.pojo.User;
 
-public interface JpaUserRepository extends JpaRepository<User, Long>{
+public interface JpaUserRepository extends JpaRepository<User, Long> {
+	List<User> findByUserNameLike(String userName);
+	User getUserById(Long id);
+	List<User> findByUserNameLikeOrNoteLike(String userName, String note);
+	
+	@Query("from user where user_name like concat('%', ?1, '%') and note like concat('%', ?2, '%')")
+	public List<User> findUsers(String userName, String note);
 }
